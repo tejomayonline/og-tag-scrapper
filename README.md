d477fd549bf2cca39e388a0acd40be529b9c4b5a
tejomayonline <tejomay.saha@srijan.net>
2021-06-09 00:49:22 +0530

    readme , basicauth added

---
+++ README.md
@@ -0,0 +1,10 @@
+# og-tag-scrapper
+
+og: Tag scrapper is a microservice with an api to scrap og tags / meta tags
+and return title, description, images in the api
+
+```sh
+POST  / 
+payload {"url": ""}
+```
+> Note: This api has basic auth enabled. without authentication it will return 401
--- app.js
+++ app.js
@@ -3,12 +3,16 @@ require('dotenv').config();
 const express = require('express');
 const helmet = require("helmet");
 const metaFinderController = require('./src/meta-finder/meta-finder.controller');
+const basicAuth = require('express-basic-auth')
 
 
 const app = express();
 const port = process.env.PORT || 3011;
 
 app.use(helmet());
+app.use(basicAuth({
+    users: { 'admin': 'supersecret' }
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
